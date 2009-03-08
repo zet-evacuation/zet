af5eb23604dd6db57fab9e939d756f779d29a632
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2009-03-08 17:19:52 +0000

    copyright notices

--- src/gui/editor/PolygonPopupListener.java
+++ src/gui/editor/PolygonPopupListener.java
@@ -1,10 +1,22 @@
+/* zet evacuation tool copyright (c) 2007-09 zet evacuation team
+ *
+ * This program is free software; you can redistribute it and/or
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 /*
  * PolygonPopupListener.java
  *
  * Created on 28. Dezember 2007, 13:11
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
  */
 
 package gui.editor;
