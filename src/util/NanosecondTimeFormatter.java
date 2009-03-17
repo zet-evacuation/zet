357b6c91772f477799f60e575bc3c03b94d2b626
mgross84 <magross@users.noreply.github.com>
2009-03-17 10:14:17 +0000

    Added conversion to minutes and comments.

--- src/util/NanosecondTimeFormatter.java
+++ src/util/NanosecondTimeFormatter.java
@@ -13,18 +13,31 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
 /*
  * NanosecondTimeFormatter.java
  *
  */
-
 package util;
 
 /**
- *
+ * An utility class to convert and format a time specified in nanoseconds into 
+ * a string using an appropriate time unit. This class is mainly used to convert
+ * algorithm runtimes measured by <code>System.nanoTime()</code> calls into a 
+ * more human readable format.
+ * 
+ * @author Martin Groß
  */
 public class NanosecondTimeFormatter {
-    
+
+    /**
+     * Converts a time specified in nanoseconds into a time unit appropriate for 
+     * the length of the specified time and formats it as a human readable 
+     * string. For example, 1445997106 ns would be converted and formatted to
+     * "1.445 s".
+     * @param timeInNanoseconds the time in nanoseconds.
+     * @return the converted and formatted string.
+     */
     public static String formatTime(long timeInNanoseconds) {
         long time = timeInNanoseconds;
         int counter = 0;
@@ -35,13 +48,24 @@ public class NanosecondTimeFormatter {
             counter++;
         }
         switch (counter) {
-            case 0: return String.format("%1$s ns", time);
-            case 1: return String.format("%1$s.%2$03d ms", time, last);// habe hier was geändet, ka was vorher da stand, gab einen utf-8-fehler also "...d?s"
-            case 2: return String.format("%1$s.%2$03d ms", time, last);
-            case 3: return String.format("%1$s.%2$03d s", time, last);
+            case 0:
+                return String.format("%1$s ns", time);
+            case 1:
+                return String.format("%1$s.%2$03d µs", time, last);
+            case 2:
+                return String.format("%1$s.%2$03d ms", time, last);
+            case 3:
+                if (time <= 60) {
+                    return String.format("%1$s.%2$03d s", time, last);
+                } else if (time > 60) {
+                    last = (int) (time % 60);
+                    time /= 60;
+                    return String.format("%1$s.%2$02d min", time, last);
+                } else {
+                    throw new AssertionError("This should not happen.");
+                }
             default:
                 throw new AssertionError("This should not happen.");
         }
     }
-    
 }
