84b687716b5dea11a74736baeb67caee7f09c3ac
saavik2001 <saavik2001@ffa672e2-ec0e-4eb3-9691-46ca224d4598>
2009-05-26 14:01:57 +0000

    I forgot to delete the debug code. It is now removed.

--- zet/src/converter/RoomRaster.java
+++ zet/src/converter/RoomRaster.java
@@ -125,7 +125,6 @@ public class RoomRaster<T extends RoomRasterSquare> extends Raster<T, Room> {
 	public LinkedList<T> getAccessibleSquares () {
 		LinkedList<T> result = new LinkedList<T> ();
 		if (rasterSquares.length==0){
-			System.out.println(p);
 			return result;
 		}		for (int j = 0; j < rasterSquares[0].length; j++) {
 			for (int i = 0; i < rasterSquares.length; i++) {
