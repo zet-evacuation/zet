c6491cf5ccce36c7e3d2cf9bd6d7afeef16bc805
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2010-01-26 21:29:14 +0000

    IO xtream files

---
+++ src/io/z/ErlangDistributionConverter.java
@@ -0,0 +1,19 @@
+/**
+ * ErlangDistributionConverter.java
+ * input:
+ * output:
+ *
+ * method:
+ *
+ * Created: Jan 26, 2010,6:48:26 PM
+ */
+package io.z;
+
+
+/**
+ *
+ * @author Jan-Philipp Kappmeier
+ */
+public class ErlangDistributionConverter {
+
+}
