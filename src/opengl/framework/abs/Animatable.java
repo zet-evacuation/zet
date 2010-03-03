a3e8179746e417319d61908515239f164fa2fc54
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2010-03-03 23:17:06 +0000

    animatable class added

---
+++ src/opengl/framework/abs/Animatable.java
@@ -0,0 +1,15 @@
+/*
+ * Drawable.java
+ * Created: 03.03.2010 17:05:50
+ */
+
+package opengl.framework.abs;
+
+/**
+ *
+ * @author Jan-Philipp Kappmeier
+ */
+public interface Animatable {
+	void animate();
+	// TODO: more methods to the interface
+}
