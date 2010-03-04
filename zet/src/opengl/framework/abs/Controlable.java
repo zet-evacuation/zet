f4000f443be140434fca4420b5b32b45be6fef14
kpdsw <kpdsw@ffa672e2-ec0e-4eb3-9691-46ca224d4598>
2010-03-04 19:01:26 +0000

    Controlable.java added

---
+++ zet/src/opengl/framework/abs/Controlable.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Controlable.java
+ * Created 04.03.2010, 18:48:16
+ */
+
+package opengl.framework.abs;
+
+/**
+ *
+ * @author Jan-Philipp Kappmeier
+ */
+public interface Controlable {
+
+}
