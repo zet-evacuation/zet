578bf14c9574266e9de8275d83a38b6b0ff8bb9e
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2010-05-25 14:53:03 +0000

    Tupel datastructure!

---
+++ src/de/tu_berlin/math/coga/datastructure/Tupel.java
@@ -0,0 +1,25 @@
+/**
+ * Tupel.java
+ * input:
+ * output:
+ *
+ * method:
+ *
+ * Created: May 18, 2010,12:51:04 PM
+ */
+package de.tu_berlin.math.coga.datastructure;
+
+
+/**
+ *
+ * @author Jan-Philipp Kappmeier
+ */
+public class Tupel<U,V> {
+	public U u;
+	public V v;
+
+	public Tupel( U u, V v ) {
+		this.u = u;
+		this.v = v;
+	}
+}
