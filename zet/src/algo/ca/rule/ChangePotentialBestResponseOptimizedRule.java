9039283e4938340a3d33015395ee06310038f71e
kpdsw <kpdsw@ffa672e2-ec0e-4eb3-9691-46ca224d4598>
2010-06-04 14:43:26 +0000

    Best Response

--- zet/src/algo/ca/rule/ChangePotentialBestResponseOptimizedRule.java
+++ zet/src/algo/ca/rule/ChangePotentialBestResponseOptimizedRule.java
@@ -17,7 +17,6 @@ package algo.ca.rule;
 
 import java.util.ArrayList;
 import ds.ca.Cell;
-import ds.ca.ExitCell;
 import ds.ca.Individual;
 import ds.ca.StaticPotential;
 import ds.ca.Room;
@@ -26,94 +25,88 @@ import ds.ca.Room;
  *
  * @author Joscha
  */
-public class ChangePotentialBestResponseOptimizedRule extends AbstractPotentialChangeRule{
-	
+public class ChangePotentialBestResponseOptimizedRule extends AbstractPotentialChangeRule {
+
 	private static final double QUEUEING_TIME_WEIGHT_FACTOR = 0.5;
-        private static final double MOVING_TIME_WEIGHT_FACTOR = 0.5;
-        private static final int TIME_STEP_LIMIT_FOR_NASH_EQUILIBRIUM = 25;
-        
+	private static final double MOVING_TIME_WEIGHT_FACTOR = 0.5;
+	private static final int TIME_STEP_LIMIT_FOR_NASH_EQUILIBRIUM = 25;
+
 	/**
 	 * 
 	 * @param cell
 	 * @return true if the potential change rule can be used
 	 */
 	@Override
-	public boolean executableOn(Cell cell) {
-            int timeStep = this.caController().getCA().getTimeStep();
-            return ((timeStep < TIME_STEP_LIMIT_FOR_NASH_EQUILIBRIUM) & (cell.getIndividual() != null)) ? true : false;
-            
-	}
-        
-        private double getResponse(Cell cell, StaticPotential pot){
-            
-            // Constants
-            Individual ind = cell.getIndividual();
-            double speed = ind.getCurrentSpeed();            
+	public boolean executableOn( Cell cell ) {
+		if( true ) 
+			throw new IllegalStateException();
+		int timeStep = this.caController().getCA().getTimeStep();
+		return ((timeStep < TIME_STEP_LIMIT_FOR_NASH_EQUILIBRIUM) & (cell.getIndividual() != null)) ? true : false;
 
-            // Exit dependant values                        
-            double distance = Double.MAX_VALUE;
-            if (pot.getDistance(cell) >= 0){
-                distance = pot.getDistance(cell);
-            }
-            double movingTime = distance / speed;                  
-            
-            double exitCapacity = this.caController().getCA().getExitToCapacityMapping().get(pot).doubleValue();    
-            //System.out.println("Exit: " + pot.getID() + " : " + exitCapacity);            
-            
-            // calculate number of individuals that are heading to the same exit and closer to it            
-            ArrayList<Individual> otherInds = new ArrayList<Individual>();
-            //cell.getRoom().getIndividuals();
-            ArrayList<Room> rooms = new ArrayList<Room>();
-            rooms.addAll(this.caController().getCA().getRooms());
-            for (Room room : rooms){
-                for (Individual i : room.getIndividuals()){
-                    otherInds.add(i);
-                }                
-            }
-            
-            int queueLength = 0;
-            if (otherInds != null)
-                for (Individual otherInd : otherInds){
-                    if (!otherInd.equals(ind)){
-                        if (otherInd.getStaticPotential() == pot){                            
-                            if (otherInd.getStaticPotential().getDistance(otherInd.getCell()) >= 0)
-                                if (otherInd.getStaticPotential().getDistance(otherInd.getCell()) < distance){
-                                    queueLength++;
-                                }
-                        }
-                    }
-                }           
-                //System.out.println("Potential = " + pot.getID());
-                //System.out.println("Queue / Kapa = " + queueLength + " / " + exitCapacity + " = " + (queueLength / exitCapacity));
-                //System.out.println("Dist / Speed = " + distance + " / " + speed + " = " + (distance / speed));
-            
-            // calculateEstimatedEvacuationTime
-            return responseFunction1(queueLength,exitCapacity,movingTime);
-            
-        }
-        
-        private double responseFunction1(int queueLength, double exitCapacity, double movingTime){
-            return (QUEUEING_TIME_WEIGHT_FACTOR * (queueLength / exitCapacity)) + (MOVING_TIME_WEIGHT_FACTOR * movingTime);
-        }        
+	}
+
+	private double getResponse( Cell cell, StaticPotential pot ) {
+
+		// Constants
+		Individual ind = cell.getIndividual();
+		double speed = ind.getCurrentSpeed();
+
+		// Exit dependant values
+		double distance = Double.MAX_VALUE;
+		if( pot.getDistance( cell ) >= 0 )
+			distance = pot.getDistance( cell );
+		double movingTime = distance / speed;
+
+		double exitCapacity = this.caController().getCA().getExitToCapacityMapping().get( pot ).doubleValue();
+		//System.out.println("Exit: " + pot.getID() + " : " + exitCapacity);
+
+		// calculate number of individuals that are heading to the same exit and closer to it
+		ArrayList<Individual> otherInds = new ArrayList<Individual>();
+		//cell.getRoom().getIndividuals();
+		ArrayList<Room> rooms = new ArrayList<Room>();
+		rooms.addAll( this.caController().getCA().getRooms() );
+		for( Room room : rooms )
+			for( Individual i : room.getIndividuals() )
+				otherInds.add( i );
+
+		int queueLength = 0;
+		if( otherInds != null )
+			for( Individual otherInd : otherInds )
+				if( !otherInd.equals( ind ) )
+					if( otherInd.getStaticPotential() == pot )
+						if( otherInd.getStaticPotential().getDistance( otherInd.getCell() ) >= 0 )
+							if( otherInd.getStaticPotential().getDistance( otherInd.getCell() ) < distance )
+								queueLength++;
+		//System.out.println("Potential = " + pot.getID());
+		//System.out.println("Queue / Kapa = " + queueLength + " / " + exitCapacity + " = " + (queueLength / exitCapacity));
+		//System.out.println("Dist / Speed = " + distance + " / " + speed + " = " + (distance / speed));
+
+		// calculateEstimatedEvacuationTime
+		return responseFunction1( queueLength, exitCapacity, movingTime );
+
+	}
+
+	private double responseFunction1( int queueLength, double exitCapacity, double movingTime ) {
+		return (QUEUEING_TIME_WEIGHT_FACTOR * (queueLength / exitCapacity)) + (MOVING_TIME_WEIGHT_FACTOR * movingTime);
+	}
 
 	/**
 	 * 
 	 * @param cell
 	 */
 	@Override
-	protected void onExecute(Cell cell) {                                   
-                        
-            ArrayList<StaticPotential> exits = new ArrayList<StaticPotential>();
-            exits.addAll(this.caController().getCA().getPotentialManager().getStaticPotentials());            
-            StaticPotential newPot = cell.getIndividual().getStaticPotential();
-            double response = Double.MAX_VALUE;
-            for (StaticPotential pot : exits){                
-                if (getResponse(cell,pot) < response){
-                    response = getResponse(cell,pot);
-                    newPot = pot;
-                }
-            }
-            cell.getIndividual().setStaticPotential(newPot);            
-            
+	protected void onExecute( Cell cell ) {
+
+		ArrayList<StaticPotential> exits = new ArrayList<StaticPotential>();
+		exits.addAll( this.caController().getCA().getPotentialManager().getStaticPotentials() );
+		StaticPotential newPot = cell.getIndividual().getStaticPotential();
+		double response = Double.MAX_VALUE;
+		for( StaticPotential pot : exits )
+			if( getResponse( cell, pot ) < response ) {
+				response = getResponse( cell, pot );
+				newPot = pot;
+			}
+		cell.getIndividual().setStaticPotential( newPot );
+
 	}
 }
--- zet/src/exitdistributions/ExitCapacityBasedCAFactory.java
+++ zet/src/exitdistributions/ExitCapacityBasedCAFactory.java
@@ -17,7 +17,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package exitdistributions;
 
 import converter.ZToCAConverter;
@@ -31,33 +30,30 @@ import evacuationplan.BidirectionalNodeCellMapping.CAPartOfMapping;
 
 /**
  *
- * @author Joscha
+ * @author Joscha Kulbatzki
  */
-public class ExitCapacityBasedCAFactory  extends ZToCAConverter{
-    
-    private static ExitCapacityBasedCAFactory instance=null;
-    
-    private static GraphBasedExitToCapacityMapping graphBasedExitToCapacityMapping = null;
-    
-    protected ExitCapacityBasedCAFactory(){        
-    }
-    
-    public CellularAutomaton convertAndApplyConcreteAssignment( BuildingPlan buildingPlan, NetworkFlowModel model, ConcreteAssignment concreteAssignment, ZToGraphRasterContainer graphRaster ) throws converter.ZToCAConverter.ConversionNotSupportedException {        
-        CellularAutomaton ca = super.convert(buildingPlan);
-        CAPartOfMapping caPartOfMapping = this.getLatestCAPartOfNodeCellMapping();
-        applyConcreteAssignment(concreteAssignment);
-        BidirectionalNodeCellMapping nodeCellMapping = new BidirectionalNodeCellMapping(graphRaster, caPartOfMapping);                              
-        graphBasedExitToCapacityMapping = new GraphBasedExitToCapacityMapping(ca, nodeCellMapping, model);
-        graphBasedExitToCapacityMapping.calculate();
-        ca.setExitToCapacityMapping( graphBasedExitToCapacityMapping.getExitCapacity() );
-        return ca;
-    }
-    
-    public static  ExitCapacityBasedCAFactory getInstance(){
-        if (instance==null){
-                instance = new ExitCapacityBasedCAFactory();
-        }
-        return instance;
-    }
+public class ExitCapacityBasedCAFactory extends ZToCAConverter {
 
+	private static ExitCapacityBasedCAFactory instance = null;
+	private static GraphBasedExitToCapacityMapping graphBasedExitToCapacityMapping = null;
+
+	protected ExitCapacityBasedCAFactory() {
+	}
+
+	public CellularAutomaton convertAndApplyConcreteAssignment( BuildingPlan buildingPlan, NetworkFlowModel model, ConcreteAssignment concreteAssignment, ZToGraphRasterContainer graphRaster ) throws converter.ZToCAConverter.ConversionNotSupportedException {
+		CellularAutomaton ca = super.convert( buildingPlan );
+		CAPartOfMapping caPartOfMapping = this.getLatestCAPartOfNodeCellMapping();
+		applyConcreteAssignment( concreteAssignment );
+		BidirectionalNodeCellMapping nodeCellMapping = new BidirectionalNodeCellMapping( graphRaster, caPartOfMapping );
+		graphBasedExitToCapacityMapping = new GraphBasedExitToCapacityMapping( ca, nodeCellMapping, model );
+		graphBasedExitToCapacityMapping.calculate();
+		ca.setExitToCapacityMapping( graphBasedExitToCapacityMapping.getExitCapacity() );
+		return ca;
+	}
+
+	public static ExitCapacityBasedCAFactory getInstance() {
+		if( instance == null )
+			instance = new ExitCapacityBasedCAFactory();
+		return instance;
+	}
 }
