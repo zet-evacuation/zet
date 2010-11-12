dff9781c1a51ffd1ea9db9ded5d73119d92907e1
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2010-11-12 15:19:54 +0000

    new localization classes and loc files

---
+++ src/de/tu_berlin/math/coga/common/localization/ZLocalization.java
@@ -0,0 +1,33 @@
+/**
+ * ZLocalization.java
+ * Created: Nov 12, 2010, 2:32:12 PM
+ */
+package de.tu_berlin.math.coga.common.localization;
+
+import java.util.MissingResourceException;
+
+/**
+ *
+ * @author Jan-Philipp Kappmeier
+ */
+public class ZLocalization extends Localization {
+	private volatile static ZLocalization singleton;
+
+	private ZLocalization() throws MissingResourceException {
+		super( "ds.z.zevacuate" );
+	}
+
+	// synchronized keyword has been removed from here
+	public static ZLocalization getSingleton() {
+		// needed because once there is singleton available no need to acquire
+		// monitor again & again as it is costly
+		if( singleton == null )
+			synchronized( ZLocalization.class ) {
+				// this is needed if two threads are waiting at the monitor at the
+				// time when singleton was getting instantiated
+				if( singleton == null )
+					singleton = new ZLocalization();
+			}
+		return singleton;
+	}
+}
