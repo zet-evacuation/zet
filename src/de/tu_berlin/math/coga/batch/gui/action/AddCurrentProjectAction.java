652514cec9bcbdb2e5b21df568ccc648cfb1577e
mgross84 <magross@users.noreply.github.com>
2011-11-17 11:47:48 +0000

    Reworking of batch view.

---
+++ src/de/tu_berlin/math/coga/batch/gui/action/AddCurrentProjectAction.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.tu_berlin.math.coga.batch.gui.action;
+
+import de.tu_berlin.math.coga.batch.gui.JBatch;
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ * @author gross
+ */
+public class AddCurrentProjectAction extends BatchAction {
+
+    public AddCurrentProjectAction(JBatch batch) {
+        super(batch, "Add current project", "box_24.png");
+    }   
+    
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }    
+}
---
+++ src/de/tu_berlin/math/coga/batch/gui/action/RunComputationAction.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.tu_berlin.math.coga.batch.gui.action;
+
+import de.tu_berlin.math.coga.batch.gui.JBatch;
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ * @author gross
+ */
+public class RunComputationAction extends BatchAction {
+
+    public RunComputationAction(JBatch batch) {
+        super(batch, "Run computation", "play_24.png");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }   
+    
+}
