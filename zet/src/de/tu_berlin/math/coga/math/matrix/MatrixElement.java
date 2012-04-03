f5301d09a0240671a49e19c44555552451c5bae8
kpdsw <kpdsw@ffa672e2-ec0e-4eb3-9691-46ca224d4598>
2012-04-03 22:00:36 +0000

    simple sparse matrix implementation

---
+++ zet/src/de/tu_berlin/math/coga/math/matrix/MatrixElement.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.tu_berlin.math.coga.math.matrix;
+
+import de.tu_berlin.math.coga.datastructure.Triple;
+
+/**
+ *
+ * @author Jan-Philipp
+ */
+public class MatrixElement {
+	private MatrixElement xNext;
+	private MatrixElement yNext;
+	private Triple<Integer,Integer,Integer> data;
+
+	public MatrixElement( Triple<Integer, Integer, Integer> data ) {
+		this.data = data;
+	}
+	
+	public void xAdd( MatrixElement xNext ) {
+		this.xNext = xNext;
+	}
+	
+	public void yAdd( MatrixElement yNext ) {
+		this.yNext = yNext;
+	}
+	
+	public MatrixElement xNext() {
+		return xNext;
+	}
+	
+	public MatrixElement yNext() {
+		return yNext;
+	}
+	
+	public int row() {
+		return data.u;
+	}
+	
+	public int col() {
+		return data.v;
+	}
+	
+	public int val() {
+		return data.w;
+	}
+
+	public String toString() {
+		return "(" + data.u + "," + data.v + "," + data.w + ")";
+	}
+	
+	public MatrixElement xLast() {
+		MatrixElement e = this;
+		while( e.xNext != null )
+			e = e.xNext;
+		return e;
+	}
+	
+	public MatrixElement yLast() {
+		MatrixElement e = this;
+		while( e.yNext != null )
+			e = e.yNext;
+		return e;
+	}
+	
+}
