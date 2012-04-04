4afbe7642ab2964cecd9be0648584d6107388b9e
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2012-04-04 16:27:24 +0000

    s

--- src/io/visualization/BuildingResults.java
+++ src/io/visualization/BuildingResults.java
@@ -25,7 +25,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import ds.z.PlanPoint;
-import ds.z.PlanPolygon.RelativePosition;
+import ds.z.RelativePosition;
 import java.util.Vector;
 
 /**
