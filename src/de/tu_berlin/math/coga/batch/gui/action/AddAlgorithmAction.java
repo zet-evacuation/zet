87f47dbc78553b939ff4022f64bc1af5a711d945
mgross84 <magross@users.noreply.github.com>
2012-05-10 15:56:45 +0000

    Batch.

--- src/de/tu_berlin/math/coga/batch/gui/action/AddAlgorithmAction.java
+++ src/de/tu_berlin/math/coga/batch/gui/action/AddAlgorithmAction.java
@@ -4,9 +4,8 @@
  */
 package de.tu_berlin.math.coga.batch.gui.action;
 
+import algo.graph.dynamicflow.eat.SEAAPAlgorithm;
 import de.tu_berlin.math.coga.batch.gui.JBatch;
-import de.tu_berlin.math.coga.batch.gui.dialog.AddAlgorithmWizard;
-import de.tu_berlin.math.coga.batch.input.InputFile;
 import java.awt.event.ActionEvent;
 
 /**
@@ -16,17 +15,12 @@ import java.awt.event.ActionEvent;
 public class AddAlgorithmAction extends BatchAction {
 
     public AddAlgorithmAction(JBatch batch) {
-        super(batch, "Add algorithm", "algo_24.png");
+        super(batch, "Tjandra (Optimized)", "algo_24.png");
         setEnabled(false);
     }   
     
     @Override
     public void actionPerformed(ActionEvent ae) {
-        System.out.println(batch.getComputationList().get(0).getInput().size());
-        for (InputFile file : batch.getComputationList().get(0).getInput()) {
-            System.out.println(file);
-        }
-        //AddAlgorithmWizard wizard = new AddAlgorithmWizard(batch);
-        //wizard.setVisible(true);
+        batch.addAlgorithm(SEAAPAlgorithm.class);
     }    
 }
