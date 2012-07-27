1ac61744163bdc9934b525fccc83b9e3f83e466f
kpdsw <kpdsw@ffa672e2-ec0e-4eb3-9691-46ca224d4598>
2012-07-27 13:50:12 +0000

    s

---
+++ zet/src/de/tu_berlin/math/coga/algorithm/shortestpath/searchtree/OptimalStaticSearchTreeAlgorithm.java
@@ -0,0 +1,185 @@
+/**
+ * OptimalStaticSearchTreeAlgorithm.java
+ * Created: 05.07.2012, 11:22:07
+ */
+package de.tu_berlin.math.coga.algorithm.shortestpath.searchtree;
+
+import de.tu_berlin.math.coga.common.algorithm.Algorithm;
+import de.tu_berlin.math.coga.datastructure.searchtree.OptimalStaticSearchTree;
+import ds.graph.Node;
+
+
+/**
+ *
+ * @param <T> 
+ * @author Jan-Philipp Kappmeier
+ */
+public class OptimalStaticSearchTreeAlgorithm<T extends Comparable<T>> extends Algorithm<OptimalStaticSearchTreeInstance<T>,OptimalStaticSearchTree<T>>{
+
+	private double[][] c;
+	private int[][] s;
+	
+	public OptimalStaticSearchTreeAlgorithm() {
+	}
+
+	@Override
+	protected OptimalStaticSearchTree<T> runAlgorithm( OptimalStaticSearchTreeInstance<T> problem ) {
+		OptimalStaticSearchTreeInstance<T> instance = getProblem();
+		instance.sort();
+		
+		c = new double[instance.size()+1][];
+		s = new int[instance.size()+1][];
+		
+		OptimalStaticSearchTree<T> ost = new OptimalStaticSearchTree<>( instance );
+
+		for( int i = instance.size()+1; i >= 1; --i ) {
+			c[instance.size()+1-i] = new double[i];
+			s[instance.size()+1-i] = new int[i];
+		}
+		
+		
+		log.fine( "Interval length 0" );
+		for( int j = 0; j <= instance.size(); ++j )
+			c[j][0] = 0.0;
+		
+		for( int interval = 1; interval <= instance.size(); ++interval ) {
+			log.fine( "Intervallänge " + interval );
+			for( int i = 0; i <= instance.size()-interval;++i ) {
+				log.finer( "C_" + i + "," + (i+interval) );
+				// compute C_j,i d.h. j ist startindex, j+i endindex
+				int j = i+interval;
+				// compute omega
+				double omega = 0;
+				for( int k = i+1-1; k < i+interval; ++k )
+					omega += instance.getProbability( k );
+				
+				double min2 = Double.MAX_VALUE;
+				int mink = 0;
+				
+				for( int k = 1; k <= interval; ++k ) {
+					
+					log.finer( "Check k = " + k + " also: " + (k+i) );
+					
+					double temp2 = c[i][k-1] + c[i+k][interval-k] + omega;
+					
+					if( temp2 < min2 ) {
+						min2 = temp2;
+						mink = i+k;
+					}
+					
+				}
+				c[i][interval] = min2;
+				s[i][interval] = mink;
+				log.finer( "Minimum for k=" + mink );
+			}
+		}
+		log.info( "Optimale Kosten: " + c[0][instance.size()]);
+		log.info( "Optimales k: " + s[0][instance.size()]);
+		
+		log.info( "Baue Tree auf" );
+		ost.setRoot( s[0][instance.size()], c[0][instance.size()] );
+		
+		buildTreeRec( ost, 0, instance.size() );
+		
+		return ost;
+	}
+	
+	private void buildTreeRec( OptimalStaticSearchTree<T> bt, int start, int interval ) {
+		// add nodes to the tree
+		int mink = s[start][interval];
+		Node tempRoot = bt.getNode( mink );
+
+		log.finest( "Performing search for node" + "[" + start + "," + (start+interval) + "] breaking at " + mink );
+		
+		// sind im Intervall [i,interval]
+		// Links: [start,mink-1-start]
+		// Rechts: [mink,start+interval-mink]
+		if( interval > 1 ) {
+			if( start != mink-1 ) {
+				// left child exists
+				Node leftChild = bt.getNode( s[start][mink-1-start] );
+				bt.setLeft( tempRoot, leftChild, c[start][mink-1-start] );
+				buildTreeRec( bt, start, mink-1-start );
+			}
+			if( mink < start+interval ) {
+				// right child exists
+				Node rightChild = bt.getNode( s[mink][start+interval-mink] );
+				bt.setRight( tempRoot, rightChild, c[mink][start+interval-mink] );
+				buildTreeRec( bt, mink, start+interval-mink );
+			}
+
+		}
+
+	}
+	
+	
+	
+	public static void main( String[] arguments ) {
+		//Debug.setDefaultLogLevel( Level.ALL );
+		OptimalStaticSearchTreeInstance<Integer> instance = new OptimalStaticSearchTreeInstance<>();
+		instance.addKeyValuePair( 1, 0.1 );
+		instance.addKeyValuePair( 2, 0.1 );
+		instance.addKeyValuePair( 3, 0.2 );
+		instance.addKeyValuePair( 4, 0.05 );
+		instance.addKeyValuePair( 5, 0.3 );
+		instance.addKeyValuePair( 6, 0.25 );
+
+		
+		OptimalStaticSearchTreeAlgorithm<Integer> alg = new OptimalStaticSearchTreeAlgorithm<>();
+		alg.setProblem( instance );
+		alg.run();		
+		System.out.println( alg.getSolution() );
+
+		instance = new OptimalStaticSearchTreeInstance<>();
+		instance.addKeyValuePair( 1, 0.2 );
+		instance.addKeyValuePair( 2, 0.05 );
+		instance.addKeyValuePair( 3, 0.1 );
+		instance.addKeyValuePair( 4, 0.25 );
+		instance.addKeyValuePair( 5, 0.1 );
+		instance.addKeyValuePair( 6, 0.3 );
+//	
+		alg = new OptimalStaticSearchTreeAlgorithm<>();
+		alg.setProblem( instance );
+		alg.run();		
+		System.out.println( alg.getSolution() );
+		
+		instance = new OptimalStaticSearchTreeInstance<>();
+		instance.addKeyValuePair( 1, 0.020 );
+		instance.addKeyValuePair( 2, 0.025 );
+		instance.addKeyValuePair( 3, 0.205 );
+		instance.addKeyValuePair( 4, 0.5 );
+		instance.addKeyValuePair( 5, 0.205 );
+		instance.addKeyValuePair( 6, 0.025 );
+		instance.addKeyValuePair( 7, 0.02 );
+		
+		alg = new OptimalStaticSearchTreeAlgorithm<>();
+		alg.setProblem( instance );
+		alg.run();		
+		System.out.println( alg.getSolution() );
+		instance = new OptimalStaticSearchTreeInstance<>();
+		instance.addKeyValuePair( 4, 0.5 );
+		instance.addKeyValuePair( 2, 0.025 );
+		instance.addKeyValuePair( 6, 0.025 );
+		instance.addKeyValuePair( 3, 0.205 );
+		instance.addKeyValuePair( 5, 0.205 );
+		instance.addKeyValuePair( 1, 0.02 );
+		instance.addKeyValuePair( 7, 0.02 );
+		
+		alg = new OptimalStaticSearchTreeAlgorithm<>();
+		alg.setProblem( instance );
+		alg.run();		
+		System.out.println( alg.getSolution() );
+		
+		instance = new OptimalStaticSearchTreeInstance<>();
+		final int c = 15;
+		for( int i = 0; i < c; ++i ) {
+			instance.addKeyValuePair( i, 1./c );
+		}
+		
+		alg = new OptimalStaticSearchTreeAlgorithm<>();
+		alg.setProblem( instance );
+		alg.run();		
+		System.out.println( alg.getSolution() );
+		
+	}
+}
---
+++ zet/src/de/tu_berlin/math/coga/algorithm/shortestpath/searchtree/OptimalStaticSearchTreeInstance.java
@@ -0,0 +1,45 @@
+/**
+ * OptimalStaticSearchTreeInstance.java
+ * Created: 05.07.2012, 11:23:37
+ */
+package de.tu_berlin.math.coga.algorithm.shortestpath.searchtree;
+
+import de.tu_berlin.math.coga.batch.algorithm.Algorithms;
+import de.tu_berlin.math.coga.datastructure.ArrayList;
+import de.tu_berlin.math.coga.datastructure.KeyValuePair;
+import java.util.Collections;
+
+
+/**
+ *
+ * @param <T> 
+ * @author Jan-Philipp Kappmeier
+ */
+public class OptimalStaticSearchTreeInstance<T extends Comparable<T>> {
+	double total = 0;
+	ArrayList<KeyValuePair<T,Double>> keys;
+
+	public OptimalStaticSearchTreeInstance() {
+		keys = new ArrayList<>();
+	}
+	
+	public void addKeyValuePair( T key, double probability ) {
+		//this.key.add( key );
+		keys.add( new KeyValuePair<>( key, probability ) );
+		total += probability;
+		if( total > 1 )
+			throw new IllegalStateException( "Total probability > 1" );
+	}
+	
+	public int size() {
+		return keys.size();
+	}
+	
+	public double getProbability( int i ) {
+		return keys.get( i ).getValue();
+	}
+	
+	public void sort() {
+		Collections.sort( keys );
+	}
+}
