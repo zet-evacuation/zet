0601e94f03757d37a8a036fd469048d78259a3ec
mgross84 <magross@users.noreply.github.com>
2012-10-10 16:00:32 +0000

    Iterable Parameters.

---
+++ src/de/tu_berlin/math/coga/datastructure/mapping/IdentifiableIntegerObjectMapping.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.tu_berlin.math.coga.datastructure.mapping;
+
+/**
+ *
+ * @author gross
+ */
+public class IdentifiableIntegerObjectMapping<T> {
+
+}
