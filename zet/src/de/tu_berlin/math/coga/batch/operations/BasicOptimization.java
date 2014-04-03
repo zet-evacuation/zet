cd7b8e3d72e422b138abfe19afde848ac109af1b
kpdsw <kpdsw@ffa672e2-ec0e-4eb3-9691-46ca224d4598>
2014-04-03 16:09:26 +0000

    converter plugins

--- zet/src/de/tu_berlin/math/coga/batch/operations/BasicOptimization.java
+++ zet/src/de/tu_berlin/math/coga/batch/operations/BasicOptimization.java
@@ -6,9 +6,17 @@ package de.tu_berlin.math.coga.batch.operations;
 
 import algo.graph.dynamicflow.eat.EarliestArrivalFlowProblem;
 import de.tu_berlin.math.coga.batch.input.reader.InputFileReader;
+import de.tu_berlin.math.coga.common.algorithm.Algorithm;
+import de.tu_berlin.math.coga.zet.converter.graph.GraphAssignmentConverter;
 import de.tu_berlin.math.coga.zet.converter.graph.NetworkFlowModel;
+import ds.GraphVisualizationResults;
+import ds.PropertyContainer;
 import ds.graph.flow.PathBasedFlowOverTime;
+import ds.z.BuildingPlan;
+import ds.z.ConcreteAssignment;
 import ds.z.Project;
+import gui.GraphConverterAlgorithms;
+import zet.tasks.GraphAlgorithmEnumeration;
 
 
 /**
@@ -17,16 +25,18 @@ import ds.z.Project;
  */
 public class BasicOptimization extends AbstractOperation implements Operation {
 	InputFileReader<Project> input;
+		AtomicOperation<BuildingPlan, NetworkFlowModel> transformationOperation;
+		AtomicOperation<EarliestArrivalFlowProblem, PathBasedFlowOverTime> eafAlgorithm;
 
 	public BasicOptimization() {
 		// First, we go from zet to network flow model
 		// then, we go from nfm to path based flow
 
-		AtomicOperation<Project, NetworkFlowModel> transformationOperation = new AtomicOperation<>( "Transformation", Project.class, NetworkFlowModel.class );
+		transformationOperation = new AtomicOperation<>( "Transformation", BuildingPlan.class, NetworkFlowModel.class );
 
 		this.addOperation( transformationOperation );
 
-		AtomicOperation<EarliestArrivalFlowProblem, PathBasedFlowOverTime> eafAlgorithm = new AtomicOperation<>( "Flow Computation", EarliestArrivalFlowProblem.class, PathBasedFlowOverTime.class );
+		eafAlgorithm = new AtomicOperation<>( "Flow Computation", EarliestArrivalFlowProblem.class, PathBasedFlowOverTime.class );
 
 		this.addOperation( eafAlgorithm );
 	}
@@ -53,9 +63,63 @@ public class BasicOptimization extends AbstractOperation implements Operation {
 
 	@Override
 	public void run() {
-		Project p = input.getSolution();
+		Project project = input.getSolution();
 
-		System.out.println( p );
+		System.out.println( project );
+
+
+
+		if( !project.getBuildingPlan().isRastered() ) {
+			System.out.print( "Building is not rasterized. Rastering... " );
+			project.getBuildingPlan().rasterize();
+			System.out.println( " done." );
+		}
+
+		if( transformationOperation.getSelectedAlgorithm() == null ) {
+			System.out.println( "No algorithm selected!");
+			return;
+		}
+		System.out.println( "Selected algorithm: " + transformationOperation.getSelectedAlgorithm() );
+
+		//if( true ) return;
+
+
+		// Convert
+		//GraphConverterAlgorithms last = GraphConverterAlgorithms.NonGridGraph;
+		final Algorithm<BuildingPlan,NetworkFlowModel> conv = transformationOperation.getSelectedAlgorithm();
+		conv.setProblem( project.getBuildingPlan() );
+		conv.run();
+		NetworkFlowModel networkFlowModel;
+
+
+		//if( networkFlowModel == null ) {
+			conv.setProblem( project.getBuildingPlan() );
+			conv.run();
+			networkFlowModel = conv.getSolution();
+		//}
+
+		// convert and create the concrete assignment
+		ConcreteAssignment concreteAssignment = project.getCurrentAssignment().createConcreteAssignment( 400 );
+
+		GraphAssignmentConverter cav = new GraphAssignmentConverter( networkFlowModel );
+
+		cav.setProblem( concreteAssignment );
+		cav.run();
+		networkFlowModel = cav.getSolution();
+
+
+		// call the graph algorithm
+		int maxTime = (int) PropertyContainer.getInstance().getAsDouble( "algo.ca.maxTime" );
+		Algorithm<NetworkFlowModel, PathBasedFlowOverTime> gt;
+		GraphAlgorithmEnumeration graphAlgorithm = GraphAlgorithmEnumeration.SuccessiveEarliestArrivalAugmentingPathOptimized;
+
+		gt = graphAlgorithm.createTask( cav.getSolution(), maxTime );
+		gt.setProblem( cav.getSolution() );
+		//gt.addAlgorithmListener( this );
+		gt.run();
+
+		// create graph vis result
+		GraphVisualizationResults gvr = new GraphVisualizationResults( cav.getSolution(), gt.getSolution() );
 	}
 
 
