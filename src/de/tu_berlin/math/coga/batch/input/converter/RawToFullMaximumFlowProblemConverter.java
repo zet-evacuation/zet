05ddb4ec61ff865eaf3481b0f3981bd72d3ae4b3
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2014-07-18 19:51:54 +0000

    dimacs reader

--- src/de/tu_berlin/math/coga/batch/input/converter/RawToFullMaximumFlowProblemConverter.java
+++ src/de/tu_berlin/math/coga/batch/input/converter/RawToFullMaximumFlowProblemConverter.java
@@ -1,7 +1,4 @@
-/*
- * RawToFullMaximumFlowProblemConverter.java
- * 
- */
+
 package de.tu_berlin.math.coga.batch.input.converter;
 
 import de.tu_berlin.coga.common.algorithm.Algorithm;
@@ -12,28 +9,29 @@ import ds.graph.problem.RawMaximumFlowProblem;
 import de.tu_berlin.coga.container.mapping.IdentifiableIntegerMapping;
 
 /**
- * Converter that transforms memory-efficient raw maximum flow problems into
- * more verbose maximum flow problems that are based on full graphs.
+ * Converter that transforms memory-efficient raw maximum flow problems into more verbose maximum flow problems that are
+ * based on full graphs.
  *
  * @author Martin Groß
  */
-public class RawToFullMaximumFlowProblemConverter extends Algorithm<RawMaximumFlowProblem,MaximumFlowProblem> {
+public class RawToFullMaximumFlowProblemConverter extends Algorithm<RawMaximumFlowProblem, MaximumFlowProblem> {
 
-    /**
-     * {@InheritDoc}
-     */
-    @Override
-    protected MaximumFlowProblem runAlgorithm(RawMaximumFlowProblem problem) {
-        DefaultDirectedGraph network = new DefaultDirectedGraph(problem.getNumberOfNodes(), problem.getNumberOfEdges());
-        IdentifiableIntegerMapping<Edge> capacities = new IdentifiableIntegerMapping<>(problem.getNumberOfEdges());
-        for (int nodeIndex = 0; nodeIndex < problem.getNumberOfNodes(); nodeIndex++) {
-            int nextStartIndex = ((nodeIndex+1 < problem.getNumberOfNodes())? problem.getNodeStartIndices()[nodeIndex+1] : problem.getNumberOfNodes());
-            for (int edgeOfNodeIndex = problem.getNodeStartIndices()[nodeIndex]; edgeOfNodeIndex < nextStartIndex; edgeOfNodeIndex++) {
-                Edge edge = network.createAndSetEdge(network.getNode(nodeIndex), network.getNode(problem.getEdgeEndIDs()[edgeOfNodeIndex]));
-                capacities.set(edge, problem.getEdgeCapacities()[edge.id()]);
-            }
-        }        
-        MaximumFlowProblem solution = new MaximumFlowProblem(network, capacities, network.getNode(problem.getSourceID()), network.getNode(problem.getSinkID()));
-        return solution;
-    }    
+  /**
+   * {@InheritDoc}
+   * @return the converted maximum flow problem
+   */
+  @Override
+  protected MaximumFlowProblem runAlgorithm( RawMaximumFlowProblem problem ) {
+    DefaultDirectedGraph network = new DefaultDirectedGraph( problem.getNumberOfNodes(), problem.getNumberOfEdges() );
+    IdentifiableIntegerMapping<Edge> capacities = new IdentifiableIntegerMapping<>( problem.getNumberOfEdges() );
+    for( int nodeIndex = 0; nodeIndex < problem.getNumberOfNodes(); nodeIndex++ ) {
+      int nextStartIndex = ((nodeIndex + 1 < problem.getNumberOfNodes()) ? problem.getNodeStartIndices()[nodeIndex + 1] : problem.getNumberOfNodes());
+      for( int edgeOfNodeIndex = problem.getNodeStartIndices()[nodeIndex]; edgeOfNodeIndex < nextStartIndex; edgeOfNodeIndex++ ) {
+        Edge edge = network.createAndSetEdge( network.getNode( nodeIndex ), network.getNode( problem.getEdgeEndIDs()[edgeOfNodeIndex] ) );
+        capacities.set( edge, problem.getEdgeCapacities()[edge.id()] );
+      }
+    }
+    MaximumFlowProblem solution = new MaximumFlowProblem( network, capacities, network.getNode( problem.getSourceID() ), network.getNode( problem.getSinkID() ) );
+    return solution;
+  }
 }
---
+++ src/de/tu_berlin/math/coga/batch/input/reader/LineBasedReader.java
@@ -0,0 +1,121 @@
+/**
+ * LineBasedReader.java Created: 18.07.2014, 17:49:04
+ */
+package de.tu_berlin.math.coga.batch.input.reader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * An implementation of an {@link InputFileReader} that reads line by line.
+ * @author Jan-Philipp Kappmeier
+ */
+public abstract class LineBasedReader<T> extends InputFileReader<T> {
+
+  /**
+   * Reads the minimum cost flow problem from the specified file.
+   * @param file the file which contains the minimum cost flow problem.
+   * @return the minimum cost flow problem. Requires 8n + 12m + O(1) storage.
+   */
+  @Override
+  protected T runAlgorithm( File file ) {
+    preOperation();
+    switch( getOptimization() ) {
+      case SPEED:
+        runAlgorithmSpeed( file, false );
+        break;
+      case MEMORY:
+        runAlgorithmMemory( file, false );
+        break;
+      default:
+        throw new AssertionError( "Should not occur." );
+    }
+    return postOperation();
+  }
+
+  /**
+   * Reads the problem in a speed-optimized way. 16n + 28m + O(1) Bytes are required.
+   * @param file the file which contains the minimum cost flow problem.
+   * @param propertiesOnly whether only the number of nodes, edges and supply should be read. Much faster than reading
+   * the whole file.
+   */
+  protected void runAlgorithmSpeed( File file, boolean propertiesOnly ) {
+    String line = null;
+    int lineIndex = 0;
+    try (BufferedReader reader = new BufferedReader( new FileReader( file ) )) {
+      while( (line = reader.readLine()) != null ) {
+        ++lineIndex;
+        if( line.trim().isEmpty() ) {
+          continue;
+        }
+
+        parseLine( line );
+      }
+    } catch( AssertionError error ) {
+      System.err.println( "Error reading " + file );
+      System.err.println( error.getMessage() );
+      System.err.println( lineIndex + ": " + line );
+    } catch( IOException ex ) {
+      System.err.println( "Exception during DimacsLoader loaded file from " + file );
+    }
+  }
+
+  /**
+   * Reads the problem in a speed-optimized way. 12n + 8m + O(1) Bytes are required.
+   * @param file the file which contains the maximum flow problem.
+   * @param propertiesOnly whether only the number of nodes and edges should be read. Much faster than reading the whole
+   * file.
+   * @return the maximum flow problem. 4n + 8m + O(1) Bytes required for storage.
+   */
+  protected void runAlgorithmMemory( File file, boolean propertiesOnly ) {
+    String line = null;
+    int lineIndex = 1;
+    try (BufferedReader reader = new BufferedReader( new FileReader( file ) )) {
+      while( (line = reader.readLine()) != null ) {
+        ++lineIndex;
+        if( line.trim().isEmpty() ) {
+          continue;
+        }
+
+        parseLine( line );
+      }
+    } catch( AssertionError error ) {
+      System.err.println( "Error reading " + file );
+      System.err.println( error.getMessage() );
+      System.err.println( lineIndex + ": " + line );
+    } catch( IOException ex ) {
+      System.err.println( "Exception during DimacsLoader loaded file from " + file );
+    }
+
+    phaseComplete();
+
+    lineIndex = 1;
+    try (BufferedReader reader = new BufferedReader( new FileReader( file ) )) {
+      while( (line = reader.readLine()) != null ) {
+        ++lineIndex;
+        if( line.trim().isEmpty() ) {
+          continue;
+        }
+
+        parseLine( line );
+      }
+    } catch( AssertionError error ) {
+      System.err.println( "Error reading " + file );
+      System.err.println( error.getMessage() );
+      System.err.println( lineIndex + ": " + line );
+    } catch( IOException ex ) {
+      System.err.println( "Exception during DimacsLoader loaded file from " + file );
+    }
+  }
+
+  protected abstract void parseLine( String line );
+
+  protected abstract void phaseComplete();
+
+  protected abstract T postOperation();
+
+  protected void preOperation() {
+  }
+}
