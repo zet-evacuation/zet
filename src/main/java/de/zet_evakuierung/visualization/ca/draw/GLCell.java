497c3822437424171a16b0529c4568585cc6e454
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2020-12-13 23:09:39 +0100

    Style and formatting for cell drawable

--- src/main/java/de/zet_evakuierung/visualization/ca/draw/GLCell.java
+++ src/main/java/de/zet_evakuierung/visualization/ca/draw/GLCell.java
@@ -1,4 +1,5 @@
-/* zet evacuation tool copyright © 2007-20 zet evacuation team
+/*
+ * zet evacuation tool copyright © 2007-20 zet evacuation team
  *
  * This program is free software; you can redistribute it and/or
  * as published by the Free Software Foundation; either version 2
@@ -26,92 +27,101 @@ import org.zetool.opengl.drawingutils.GLVector;
 import org.zetool.opengl.framework.abs.AbstractDrawable;
 
 public class GLCell extends AbstractDrawable<GLCell, GLCellControl> {
-	private static GLVector ul = null;	// upper Left
-	private static GLVector ur;	// upper Right
-	private static GLVector ll;	// lower Left
-	private static GLVector lr;	// lower Right
-	protected GLColor color;
-	private GLColor defaultColor;
-	protected static GLColor wallColor = VisualizationOptionManager.getCellWallColor();
 
-	public GLCell( GLCellControl model ) {
-    //this( model, VisualizationOptionManager.getCellFloorColor() );
-    this( model, VisualizationOptionManager.getCellWallColor() );
-  }
+    /** Top left coordinate of the squre cell. */
+    private static GLVector topLeft;
+    /** Top right coordinate of the squre cell. */
+    private static GLVector topRight;
+    /** Bottom left coordinate of the squre cell. */
+    private static GLVector bottomLeft;
+    /** Bottom right coordinate of the squre cell. */
+    private static GLVector bottomRight;
+    /** Current color of the cell floor. */
+    protected GLColor color;
+    /** Default color of the cell floor. */
+    private final GLColor defaultColor;
 
-	public GLCell( GLCellControl control, GLColor color ) {
-		super( control, new GLVector(control.getXPosition(), control.getYPosition(), 0));
-		if( ul == null ) {
-			ul = new GLVector( control.getOffset(), -control.getOffset(), 0 );
-			ur = new GLVector( control.getWidth(), -control.getOffset(), 0 );
-			ll = new GLVector( control.getOffset(), -control.getWidth(), 0 );
-			lr = new GLVector( control.getWidth(), -control.getWidth(), 0 );
-		}
-		this.color = color;
-		this.defaultColor = color;
-	}
+    public GLCell(GLCellControl model) {
+        //this( model, VisualizationOptionManager.getCellFloorColor() );
+        this(model, VisualizationOptionManager.getCellWallColor());
+    }
 
-	@Override
-	public void performDynamicDrawing( GL2 gl ) {
-		if( VisualizationOptionManager.smoothCellVisualization() ) {
-			boolean lighting = gl.glIsEnabled( GL2.GL_LIGHTING );
-			gl.glBegin( GL2.GL_QUADS );
-			gl.glNormal3d( 0, 0, 1 );
-		 	getModel().mixColorWithNeighbours( Direction8.TopLeft ).draw( gl, lighting );
-			ul.draw( gl );
-			getModel().mixColorWithNeighbours( Direction8.TopRight ).draw( gl, lighting );
-			ur.draw( gl );
-			getModel().mixColorWithNeighbours( Direction8.DownLeft ).draw( gl, lighting );
-			lr.draw( gl );
-			getModel().mixColorWithNeighbours( Direction8.DownRight ).draw( gl, lighting );
-			ll.draw( gl );
-			gl.glEnd();
-		} else {
-			color.draw( gl );
-			gl.glBegin( GL2.GL_QUADS );
-			gl.glNormal3d( 0, 0, 1 );
-			ul.draw( gl );
-			ur.draw( gl );
-			lr.draw( gl );
-			ll.draw( gl );
-			gl.glEnd();
-		}
-	}
+    public GLCell(GLCellControl control, GLColor color) {
+        super(control, new GLVector(control.getXPosition(), control.getYPosition(), 0));
+        if (topLeft == null) {
+            topLeft = new GLVector(control.getOffset(), -control.getOffset(), 0);
+            topRight = new GLVector(control.getWidth(), -control.getOffset(), 0);
+            bottomLeft = new GLVector(control.getOffset(), -control.getWidth(), 0);
+            bottomRight = new GLVector(control.getWidth(), -control.getWidth(), 0);
+        }
+        this.color = color;
+        this.defaultColor = color;
+    }
 
-	/**
-	 * Updates the graphical representation of the cell. The current floor color
-	 * is calculated.
-	 */
-	protected void updateFloorColor() {
-		if( model.getDisplayMode() == CellInformationDisplay.NoPotential )
-			this.color = getDefaultColor();
-		else
-			this.color = potentialToColor( model.getCellInformation( model.getDisplayMode() ), model.getMaxCellInformation( model.getDisplayMode() ), VisualizationOptionManager.getCellInformationLowColor( model.getDisplayMode() ), VisualizationOptionManager.getCellInformationHighColor( model.getDisplayMode() ) );
-	}
+    @Override
+    public void performDynamicDrawing(GL2 gl) {
+        if (VisualizationOptionManager.smoothCellVisualization()) {
+            boolean lighting = gl.glIsEnabled(GL2.GL_LIGHTING);
+            gl.glBegin(GL2.GL_QUADS);
+            gl.glNormal3d(0, 0, 1);
+            getModel().mixColorWithNeighbours(Direction8.TopLeft).draw(gl, lighting);
+            topLeft.draw(gl);
+            getModel().mixColorWithNeighbours(Direction8.TopRight).draw(gl, lighting);
+            topRight.draw(gl);
+            getModel().mixColorWithNeighbours(Direction8.DownLeft).draw(gl, lighting);
+            bottomRight.draw(gl);
+            getModel().mixColorWithNeighbours(Direction8.DownRight).draw(gl, lighting);
+            bottomLeft.draw(gl);
+            gl.glEnd();
+        } else {
+            color.draw(gl);
+            gl.glBegin(GL2.GL_QUADS);
+            gl.glNormal3d(0, 0, 1);
+            topLeft.draw(gl);
+            topRight.draw(gl);
+            bottomRight.draw(gl);
+            bottomLeft.draw(gl);
+            gl.glEnd();
+        }
+    }
 
-	protected final GLColor potentialToColor( long potential, long maxPotential, GLColor lowColor, GLColor highColor ) {
-		if( model.isPotentialValid() )
-			return lowColor.blend( highColor, potential / (double)maxPotential );
-		else
-			return VisualizationOptionManager.getInvalidPotentialColor();
-	}
+    /**
+     * Updates the graphical representation of the cell. The current floor color is calculated.
+     */
+    protected void updateFloorColor() {
+        if (model.getDisplayMode() == CellInformationDisplay.NoPotential) {
+            color = getDefaultColor();
+        } else {
+            color = potentialToColor(model.getCellInformation(model.getDisplayMode()),
+                    model.getMaxCellInformation(model.getDisplayMode()),
+                    VisualizationOptionManager.getCellInformationLowColor(model.getDisplayMode()),
+                    VisualizationOptionManager.getCellInformationHighColor(model.getDisplayMode()));
+        }
+    }
 
-	public final GLColor getDefaultColor() {
-		return defaultColor;
-	}
+    protected final GLColor potentialToColor(long potential, long maxPotential, GLColor lowColor, GLColor highColor) {
+        if (model.isPotentialValid()) {
+            return lowColor.blend(highColor, potential / (double) maxPotential);
+        } else {
+            return VisualizationOptionManager.getInvalidPotentialColor();
+        }
+    }
 
-	@Override
-	public void update() {
-		updateFloorColor();
-	}
+    public final GLColor getDefaultColor() {
+        return defaultColor;
+    }
 
-	@Override
-	public String toString() {
-		return "GLCell";
-	}
+    @Override
+    public void update() {
+        updateFloorColor();
+    }
 
-	public GLColor getColor() {
-		return color;
-	}
+    @Override
+    public String toString() {
+        return "GLCell";
+    }
+
+    public GLColor getColor() {
+        return color;
+    }
 }
-
